73589c5e0d842e0e58e694325f86f717ae615106
thzs <thzs@ee5bfcf9-8b04-45a4-a66c-bec449605a02>
2006-05-20 22:43:45 +0000

    - creating extra branch 0.8.4_symbols

---
+++ branches/0.8.4_symbols/3rdparty/liborigin/OPJFile.h
@@ -0,0 +1,65 @@
+// OPJFile.h
+
+#ifndef OPJFILE_H
+#define OPJFILE_H
+
+// if it crashes : reduce numbers here
+#if defined(_MSC_VER) //MSVC Compiler
+#define MAX_SPREADS 128
+#define MAX_COLUMNS 128
+#define MAX_ENTRIES 8192
+#else
+#define MAX_SPREADS 1024
+#define MAX_COLUMNS 1024
+#define MAX_ENTRIES 65535
+#endif
+
+// for string entries
+struct Entry {
+	short spread;
+	short column;
+	short row;
+	char *name;
+};
+
+typedef Entry Entry;
+
+class OPJFile
+{
+public:
+	OPJFile(char* filename);
+	int Parse();
+	double Version() { return version/100.0; }		//!< get version of project file
+	int numSpreads() { return nr_spreads; }			//!< get number of spreadsheets
+	char *spreadName(int s) { return spreadname[s]; }	//!< get name of spreadsheet s
+	int numCols(int s) { return nr_cols[s]; }		//!< get number of columns of spreadsheet s
+	int numRows(int s,int c) { return nr_rows[s][c]; }	//!< get number of rows of column c of spreadsheet s
+	int maxRows(int s) { return maxrows[s]; }		//!< get maximum number of rows of spreadsheet s
+	char *colName(int s, int c) { return colname[s][c]; }	//!< get name of column c of spreadsheet s
+	char *colType(int s, int c) { return coltype[s][c]; }	//!< get type of column c of spreadsheet s
+	double* Data(int s, int c) { return data[s][c]; }	//!< get data of column c of spreadsheet s
+	char* SData(int s, int c, int r) { 
+		for (int i=0;i<MAX_ENTRIES;i++) {
+			if(sdata[i].spread != s) continue;
+			if(sdata[i].column != c) continue;
+			if(sdata[i].row != r) continue;
+			return sdata[i].name;
+		}					
+		return 0;
+	}	//!< get data strings of column c/row r of spreadsheet s
+private:
+	char* filename;				//!< project file name
+	int version;				//!< project version
+	int nr_spreads;				//!< number of spreadsheets
+	char *spreadname[MAX_SPREADS];		//!< spreadsheet names
+	int nr_cols[MAX_SPREADS];		//!< number of cols per spreadsheet
+	int nr_rows[MAX_SPREADS][MAX_COLUMNS];	//!< number of rows per column of spreadsheet
+	int maxrows[MAX_SPREADS];		//!< max number of rows of spreadsheet
+	double *data[MAX_SPREADS][MAX_COLUMNS];	//!< data per column per spreadsheet
+	Entry sdata[MAX_ENTRIES];		// should be enough
+	int entry;
+	char* colname[MAX_SPREADS][MAX_COLUMNS];	//!< column names
+	char* coltype[MAX_SPREADS][MAX_COLUMNS];	//!< column types
+};
+
+#endif // OPJFILE_H
---
+++ branches/0.8.4_symbols/qtiplot/src/importOPJ.h
@@ -0,0 +1,22 @@
+#ifndef IMPORTOPJ_H
+#define IMPORTOPJ_H
+
+#include <qobject.h>
+
+class ApplicationWindow;
+class OPJFile;
+
+class ImportOPJ : public QObject
+{
+public:
+	ImportOPJ(ApplicationWindow *mw, const QString& filename);
+	~ImportOPJ(){};
+
+	bool importTables(OPJFile opj);
+	int error(){return parse_error;};
+
+private:
+	int parse_error;
+};
+
+#endif //IMPORTOPJ_H
