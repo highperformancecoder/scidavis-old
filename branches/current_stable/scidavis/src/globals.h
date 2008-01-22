0f7c509a7ac9eb1b10cbd31b74cec561fb7b168c
thzs <thzs@ee5bfcf9-8b04-45a4-a66c-bec449605a02>
2008-01-22 22:17:31 +0000

    - added Mauricio Troviano and Burkhard Bunk to the acknowledgment string for the next version

--- branches/current_stable/scidavis/src/globals.h
+++ branches/current_stable/scidavis/src/globals.h
@@ -40,6 +40,6 @@ int scidavis_version = 0x000101;
 //! Extra version information string (like "-alpha", "-beta", "-rc1", etc...)
 const char * extra_version = "";
 //! Copyright string containing the author names etc.
-const char * copyright_string = "Developers (alphabetical order):\nKnut Franke\nTilman Hoener zu Siederdissen\n\nDocumentation:\nRoger Gadiou\n\nSpecial thanks to (alphabetical order):\nQuentin Denis\nGudjon I. Gudjonsson\nAlex Kargovsky\nIon Vasilief\n\nThanks to (no particular order):\nthe developers of Qt, Qwt, QwtPlot3D, GSL, muParser, zlib, Python, PyQt, and liborigin\nall bug reporters, translators and other contributors";
+const char * copyright_string = "Developers (alphabetical order):\nKnut Franke\nTilman Hoener zu Siederdissen\n\nDocumentation:\nRoger Gadiou\n\nSpecial thanks to (alphabetical order):\nBurkhard Bunk\nQuentin Denis\nGudjon I. Gudjonsson\nAlex Kargovsky\nMauricio Troviano\nIon Vasilief\n\nThanks to (no particular order):\nthe developers of Qt, Qwt, QwtPlot3D, GSL, muParser, zlib, Python, PyQt, and liborigin\nall bug reporters, translators and other contributors";
 //! Release date as a string
 const char * release_date = " 2007-12-21";
