1c5b6746e2702234fe2b2904f651bdc5e6e0957d
thzs <thzs@ee5bfcf9-8b04-45a4-a66c-bec449605a02>
2008-04-16 21:53:25 +0000

    - some preparations for release 0.1.3 (splash screen, globals.h, CHANGES, Doxyfile) - replaced my old last name

--- branches/current_stable/scidavis/src/main.cpp
+++ branches/current_stable/scidavis/src/main.cpp
@@ -2,7 +2,7 @@
     File                 : main.cpp
     Project              : SciDAVis
     --------------------------------------------------------------------
-    Copyright            : (C) 2006 by Ion Vasilief, Tilman Hoener zu Siederdissen
+    Copyright            : (C) 2006 by Ion Vasilief, Tilman Benkert
     Email (use @ for *)  : ion_vasilief*yahoo.fr, thzs*gmx.net
     Description          : SciDAVis main function
 
