69b46d08adbf435cbe60902d0cc2dd3f70042206
thzs <thzs@ee5bfcf9-8b04-45a4-a66c-bec449605a02>
2008-04-17 21:21:56 +0000

    - added debian directory and its contents from Burkhard Bunks latest source package

---
+++ branches/current_stable/debian/scidavis-default.ex
@@ -0,0 +1,10 @@
+# Defaults for scidavis initscript
+# sourced by /etc/init.d/scidavis
+# installed at /etc/default/scidavis by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
