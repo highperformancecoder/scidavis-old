6d94045b628c846087ae9b7d7d2f20875b2b79c8
knut_f <knut_f@ee5bfcf9-8b04-45a4-a66c-bec449605a02>
2010-03-12 14:49:20 +0000

    - Enabled docstring generation feature in SIP >= 4.10.
    - Added column access using array notation. Calls like
      t.column("xyz").setValueAt(0,3) are a bit lengthy, especially compared with
      the now-deprecated t.setCell("xyz",0,3). Adding the shorthand notation
      t["xyz"].setValueAt(0,3) will hopefully make the move towards the
      column-as-objects paradigm less painfull.

--- branches/current_stable/scidavis/python-sipcmd.py
+++ branches/current_stable/scidavis/python-sipcmd.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
 from PyQt4 import pyqtconfig
 config = pyqtconfig.Configuration()
-print " ".join([config.sip_bin, "-I", config.pyqt_sip_dir, config.pyqt_sip_flags])
+flags = []
+if config.sip_version >= 0x040a00:
+	# make use of docstring generation feature in SIP >= 4.10
+	flags.append("-o")
+print " ".join([config.sip_bin, "-I", config.pyqt_sip_dir, config.pyqt_sip_flags] + flags)
