64de7ee877031906af25d726d5c68d1d6fdd983a
knut_f <knut_f@ee5bfcf9-8b04-45a4-a66c-bec449605a02>
2010-04-01 09:39:41 +0000

    - Added copyright/license header to some files written entirely or primarily by me.

--- branches/current_stable/fitPlugins/explin/explin.c
+++ branches/current_stable/fitPlugins/explin/explin.c
@@ -1,3 +1,35 @@
+/***************************************************************************
+   File                 : planck_wavelength.c
+   Project              : SciDAVis
+   Description          : Fit plugin for "exp(-b1*x)/(b2+b3*x)"
+   --------------------------------------------------------------------
+   Copyright            : (C) 2008 Knut Franke (knut.franke*gmx.de)
+                          (replace * with @ in the email address)
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  As a special exception, you may omit the above copyright notice when   *
+ *  distributing modified copies of this file (for instance, when using it *
+ *  as a template for your own fit plugin).                                *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
 #include <math.h>
