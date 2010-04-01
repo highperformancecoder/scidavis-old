fb66d5bba834027887bb233803e5f45694828c1f
knut_f <knut_f@ee5bfcf9-8b04-45a4-a66c-bec449605a02>
2010-04-01 13:28:24 +0000

    - Added copyright/license header to files written by Ion Vasilief, after   consulting him on the matter. Compare QtiPlot r1656.

--- branches/current_stable/fitPlugins/fitRational0/fitRational0.cpp
+++ branches/current_stable/fitPlugins/fitRational0/fitRational0.cpp
@@ -1,3 +1,35 @@
+/***************************************************************************
+   File                 : fitRational0.cpp
+   Project              : SciDAVis
+   Description          : Fit plugin for "(b + c*x)/(1 + a*x)"
+   --------------------------------------------------------------------
+   Copyright            : (C) 2005 Ion Vasilief (ion_vasilief*yahoo.fr)
+						  (replace * with @ in the email address)
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  As a special exception, you may omit the above copyright notice when   *
+ *  distributing modified copies of this file (for instance, when using it *
+ *  as a template for your own fit plugin).                                *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
--- branches/current_stable/scidavis/src/fit_gsl.h
+++ branches/current_stable/scidavis/src/fit_gsl.h
@@ -1,3 +1,36 @@
+/***************************************************************************
+   File                 : fit_gsl.h
+   Project              : SciDAVis
+   Description          : Built-in data fit models for QtiPlot
+   --------------------------------------------------------------------
+   Copyright            : (C) 2004-2007 Ion Vasilief (ion_vasilief*yahoo.fr)
+                          (C) 2008-2009 Knut Franke (knut.franke*gmx.de)
+						  (replace * with @ in the email address)
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  As a special exception, you may omit the above copyright notice when   *
+ *  distributing modified copies of this file (for instance, when using it *
+ *  as a template for your own fit plugin).                                *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef FIT_GSL_H
 #define FIT_GSL_H
 
