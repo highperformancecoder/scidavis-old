768827d929a9099e853457d31a0b32833c9cdd15
Russell Standish <hpcoder@hpcoders.com.au>
2017-05-30 13:30:55 +1000

    Release 1.15

--- libscidavis/src/version.cpp
+++ libscidavis/src/version.cpp
@@ -1,4 +1,4 @@
 #include "globals.h"
-const int SciDAVis::scidavis_versionNo = 69127;
-const char* SciDAVis::scidavis_version = "1.14.7";
+const int SciDAVis::scidavis_versionNo = 69376;
+const char* SciDAVis::scidavis_version = "1.15";
 const char * SciDAVis::release_date = "May 30, 2017";
