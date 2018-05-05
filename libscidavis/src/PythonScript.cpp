b83abe957cc6d9dfbd3154e81525baaced04d88d
Miquel Garriga <gbmiquel@gmail.com>
2018-05-05 17:37:26 +0200

    Fix use of non-ASCII characters in scripts

--- libscidavis/src/PythonScript.cpp
+++ libscidavis/src/PythonScript.cpp
@@ -144,7 +144,7 @@ bool PythonScript::compile(bool for_eval)
 	bool success=false;
 	Py_XDECREF(PyCode);
 	// Simplest case: Code is a single expression
-	PyCode = Py_CompileString(Code.toLocal8Bit(), Name.toLocal8Bit(), Py_eval_input);
+	PyCode = Py_CompileString(Code.toUtf8().constData(), Name, Py_eval_input);
 	if (PyCode) {
 		success = true;
 	} else if (for_eval) {
@@ -184,7 +184,7 @@ bool PythonScript::compile(bool for_eval)
 		// Code contains statements (or errors), but we do not need to get
 		// a return value.
 		PyErr_Clear(); // silently ignore errors
-		PyCode = Py_CompileString(Code.toLocal8Bit(), Name.toLocal8Bit(), Py_file_input);
+		PyCode = Py_CompileString(Code.toUtf8().constData(), Name, Py_file_input);
 		success = PyCode != NULL;
 	}
 	if (!success)
