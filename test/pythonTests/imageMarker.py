8abae594d2797c28ca1f5cff4fd5f44d8a2c474f
Miquel Garriga <gbmiquel@gmail.com>
2018-11-23 19:09:13 +0100

    Make imageMarker python test work with PyQt5

--- test/pythonTests/imageMarker.py
+++ test/pythonTests/imageMarker.py
@@ -3,8 +3,7 @@ layer = graph.activeLayer()
 image = layer.addImage("scidavis-logo.png")
 assert image.fileName()=="scidavis-logo.png"
 image.setSize(260,100)
-from PyQt4.QtCore import QSize 
-assert image.size()==QSize(260,100)
+assert image.size()==QtCore.QSize(260,100)
 image.setCoordinates(200,800,330,850)
 layer.replot()
 graph.exportImage("imageMarker.png");
