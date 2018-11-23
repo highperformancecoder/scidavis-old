92c1cd08cef878cfd50fb7efbc0c88a012fd7a02
Russell Standish <hpcoder@hpcoders.com.au>
2018-11-23 17:38:01 +1100

    arrowMarker python test

---
+++ test/pythonTests/arrowMarker.py
@@ -0,0 +1,18 @@
+arrow = ArrowMarker()
+arrow.setStart(50,200)
+arrow.setEnd(400,400)
+arrow.setWidth(2)
+arrow.drawStartArrow(False)
+arrow.drawEndArrow(True)
+arrow.setColor(Qt.green)
+arrow.setHeadLength(10)
+arrow.setHeadAngle(20)
+arrow.fillArrowHead(True)
+
+graph=newGraph()
+layer = graph.activeLayer()
+layer.addArrow(arrow)
+layer.replot()
+
+graph.exportImage("arrowMarker.png");
+app.exit()
