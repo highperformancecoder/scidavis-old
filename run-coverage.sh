9fba0aab1d469862a692eccc4e3ceffda4b6c3a3
Russell Standish <hpcoder@hpcoders.com.au>
2018-11-25 12:59:23 +1100

    Fix up coverage test.

--- run-coverage.sh
+++ run-coverage.sh
@@ -6,8 +6,16 @@
 for i in test/00/*; do
     sh $i
 done
-lcov --rc "geninfo_adjust_src_path=tmp/" -i -c -d . --no-external -o lcovi.info
-lcov --rc "geninfo_adjust_src_path=tmp/" -c -d .  --no-external -o lcovt.info
+
+# gen_info source path rewriting no longer seems to cope with the
+# scidavis file layout, so just fake it by creating some symbolic
+# links where lcov and genhtml expect the source files to be
+ln -sf `pwd`/libscidavis/src tmp
+ln -sf `pwd`/test/*.{h,cpp} tmp/test
+dir=tmp/libscidavis
+
+lcov -i -c -d . --exclude tmp/test --no-external -o lcovi.info
+lcov -c -d . --exclude tmp/test  --no-external -o lcovt.info
 rm lcov.info
 lcov -a lcovi.info -a lcovt.info -o lcov.info
 genhtml --ignore-errors source -o coverage lcov.info
