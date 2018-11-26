ec8be80b3b22bfd7f9f403a02af0ce3c5b6de236
Russell Standish <hpcoder@hpcoders.com.au>
2018-11-26 18:16:06 +1100

    Remove redundant header file

--- test/near.h
+++
@@ -1,9 +0,0 @@
-#ifndef NEAR_H
-#define NEAR_H
-
-// performs customisable fuzzy matching of reals
-inline bool near(double x, double y, double tol)
-{return fabs(x-y)/(fabs(x)+fabs(y)) < tol;}
-
-
-#endif
