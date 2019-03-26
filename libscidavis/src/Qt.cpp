a549cee2c3e2cb39d6e336d996e815a79881e090
Russell Standish <hpcoder@hpcoders.com.au>
2019-03-26 14:26:12 +1100

    Missing file

---
+++ libscidavis/src/Qt.cpp
@@ -0,0 +1,24 @@
+#include "Qt.h"
+#include <qcolor.h>
+
+const QColor QtNamespace::white{Qt::white};
+const QColor QtNamespace::black{Qt::black};
+const QColor QtNamespace::red{Qt::red};
+const QColor QtNamespace::darkRed{Qt::darkRed};
+const QColor QtNamespace::green{Qt::green};
+const QColor QtNamespace::darkGreen{Qt::darkGreen};
+const QColor QtNamespace::blue{Qt::blue};
+const QColor QtNamespace::darkBlue{Qt::darkBlue};
+const QColor QtNamespace::cyan{Qt::cyan};
+const QColor QtNamespace::darkCyan{Qt::darkCyan};
+const QColor QtNamespace::magenta{Qt::magenta};
+const QColor QtNamespace::darkMagenta{Qt::darkMagenta};
+const QColor QtNamespace::yellow{Qt::yellow};
+const QColor QtNamespace::darkYellow{Qt::darkYellow};
+const QColor QtNamespace::gray{Qt::gray};
+const QColor QtNamespace::darkGray{Qt::darkGray};
+const QColor QtNamespace::lightGray{Qt::lightGray};
+const QColor QtNamespace::transparent{Qt::transparent};
+const QColor QtNamespace::color0{Qt::color0};
+const QColor QtNamespace::color1{Qt::color1};
+
