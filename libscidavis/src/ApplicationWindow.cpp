18fc3f6810f3fdfc914b08652c680c5d11093f26
Mirko Scholz <srtlg@users.noreply.github.com>
2019-09-30 10:57:32 +0200

    compiles under Arch (Qt 5.13.1, GCC 9.1.0)

--- libscidavis/src/ApplicationWindow.cpp
+++ libscidavis/src/ApplicationWindow.cpp
@@ -2606,7 +2606,7 @@ TableStatistics *ApplicationWindow::newTableStatistics(Table *base, int type, QL
 
 void ApplicationWindow::removeDependentTableStatistics(const AbstractAspect *aspect)
 {
-	future::Table *future_table = qobject_cast<future::Table *>(const_cast<AbstractAspect *>(aspect));
+	::future::Table *future_table = qobject_cast<::future::Table *>(const_cast<AbstractAspect *>(aspect));
 	if (!future_table) return;
 	QList<MyWidget*> windows = windowsList();
 	foreach(MyWidget *win, windows)
@@ -13891,13 +13891,13 @@ void ApplicationWindow::selectPlotType(int type)
 void ApplicationWindow::handleAspectAdded(const AbstractAspect *parent, int index)
 {
 	AbstractAspect * aspect = parent->child(index);
-	future::Matrix * matrix = qobject_cast<future::Matrix *>(aspect);
+	::future::Matrix * matrix = qobject_cast<::future::Matrix *>(aspect);
 	if (matrix)
 	{
 		initMatrix(static_cast<Matrix *>(matrix->view()));
 		return;
 	}
-	future::Table * table = qobject_cast<future::Table *>(aspect);
+	::future::Table * table = qobject_cast<::future::Table *>(aspect);
 	if (table)
 	{
 		initTable(static_cast<Table *>(table->view()));
@@ -13908,13 +13908,13 @@ void ApplicationWindow::handleAspectAdded(const AbstractAspect *parent, int inde
 void ApplicationWindow::handleAspectAboutToBeRemoved(const AbstractAspect *parent, int index)
 {
 	AbstractAspect * aspect = parent->child(index);
-	future::Matrix * matrix = qobject_cast<future::Matrix *>(aspect);
+	::future::Matrix * matrix = qobject_cast<::future::Matrix *>(aspect);
 	if (matrix)
 	{
 		closeWindow(static_cast<Matrix *>(matrix->view()));
 		return;
 	}
-	future::Table * table = qobject_cast<future::Table *>(aspect);
+	::future::Table * table = qobject_cast<::future::Table *>(aspect);
 	if (table)
 	{
 		closeWindow(static_cast<Table *>(table->view()));
@@ -13949,7 +13949,7 @@ void ApplicationWindow::showHistory()
 
 QStringList ApplicationWindow::tableWindows()
 {
-	QList<AbstractAspect *> tables = d_project->descendantsThatInherit("future::Table");
+	QList<AbstractAspect *> tables = d_project->descendantsThatInherit("::future::Table");
 	QStringList result;
 	foreach(AbstractAspect *aspect, tables)
 		result.append(aspect->name());
