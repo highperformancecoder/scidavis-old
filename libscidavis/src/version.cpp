db4b10e36d7a066a2ff86ee58b5d3c811c9a6c54
Russell Standish <hpcoder@hpcoders.com.au>
2020-01-31 17:05:06 +1100

    Release 2.0.5

--- libscidavis/src/version.cpp
+++ libscidavis/src/version.cpp
@@ -1,5 +1,5 @@
 #include "globals.h"
-const int SciDAVis::scidavis_versionNo = 131076;
-const char* SciDAVis::scidavis_version = "2.0.4";
+const int SciDAVis::scidavis_versionNo = 131077;
+const char* SciDAVis::scidavis_version = "2.0.5";
 const char* SciDAVis::extra_version = "-beta";
 const char * SciDAVis::release_date = "Jan 31, 2020";
