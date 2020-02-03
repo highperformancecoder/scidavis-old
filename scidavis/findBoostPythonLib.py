6245110451ec0dfa3202ce0822b20de196d5845a
Russell Standish <hpcoder@hpcoders.com.au>
2020-02-03 12:37:23 +1100

    Change findBoostPythonLib to work on Redhat systems.

--- scidavis/findBoostPythonLib.py
+++ scidavis/findBoostPythonLib.py
@@ -4,13 +4,16 @@ import re
 
 dirs=[os.environ['HOME']+'/usr/lib', '/usr/local/lib', '/usr/local/lib64', '/usr/lib', '/usr/lib64']
 
-pattern=re.compile("lib(boost_python-py"+str(sys.version_info.major)+".*).so$")
+#pattern1=re.compile("lib(boost_python-py"+str(sys.version_info.major)+".*).so$")
+pattern1=re.compile("lib(boost_python"+str(sys.version_info.major)+".*).so$")
 
 for dir in dirs:
     for (root,subs,files) in os.walk(dir):
         for file in files:
-            match=pattern.match(file)
+            match=pattern1.match(file)
 #            print(file,match)
+#            if match is None:
+#                match=pattern2.match(file)
             if match is not None:
                 print('-l'+match.group(1))
         
