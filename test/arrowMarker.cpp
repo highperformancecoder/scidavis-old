f675e78bcbfc905679ebea2c3746a56b2c8e66a3
Mehdi Chinoune <mehdi.chinoune@hotmail.com>
2021-01-21 12:11:04 +0100

    Unify UnitTest++ include directories.

--- test/arrowMarker.cpp
+++ test/arrowMarker.cpp
@@ -5,11 +5,7 @@
 #include <thread>
 using namespace std;
 
-#ifdef _WIN32
-#include <UnitTest++.h>
-#else
 #include <UnitTest++/UnitTest++.h>
-#endif
 #include "utils.h"
 
 static double sqr(double x)
