83a36a6ceabc7b1ccffbfbf75e2ccf75947ae41b
Russell Standish <hpcoder@hpcoders.com.au>
2021-03-07 09:30:37 +1100

    Update makeDist.sh to handle submodules.

--- makeDist.sh
+++ makeDist.sh
@@ -7,4 +7,15 @@ else
     name=scidavis-$version~$extra
 fi
 
-git archive --format=tar.gz --prefix=$name/ HEAD -o $name.tar.gz
+git archive --format=tar --prefix=$name/ HEAD -o $name.tar
+
+# add submodules
+git submodule update --init --recursive
+git submodule|cut -f3 -d' '|while read s; do
+    pushd $s
+    git archive --format=tar --prefix=$name/$s/ HEAD -o /tmp/$$.tar
+    popd
+    tar Af $name.tar /tmp/$$.tar
+done
+gzip -f $name.tar
+rm /tmp/$$.tar
