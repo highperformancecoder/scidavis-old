abcbd5a215ecc0e2c5c87371f7cdf533c7637abe
Suthiro <soviet_man@list.ru>
2021-04-17 17:51:31 +0700

    dynamic_cast of filters

--- libscidavis/src/future/table/TableView.cpp
+++ libscidavis/src/future/table/TableView.cpp
@@ -728,8 +728,16 @@ void TableView::applyType()
                 break;
             }
             col->setColumnMode(new_mode, converter);
-            static_cast<DateTime2StringFilter*>(col->outputFilter())->setFormat(format);
-            static_cast<String2DateTimeFilter*>(col->inputFilter())->setFormat(format);
+            {
+                auto filter = dynamic_cast<DateTime2StringFilter *>(col->outputFilter());
+                if (nullptr != filter)
+                    filter->setFormat(format);
+            }
+            {
+                auto filter = dynamic_cast<String2DateTimeFilter *>(col->inputFilter());
+                if (nullptr != filter)
+                    filter->setFormat(format);
+            }
             col->endMacro();
         }
         break;
